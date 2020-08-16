96ca9f5cb487d6079103f840d797249067be050c
teja8352 <tejastar123@gmail.com>
2020-08-16 20:10:46 +0530

    dashboard, test pages added

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,236 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var column_chart_container = Highcharts.chart('column-chart-container', {
-        chart: {
-            type: 'column',
-            renderTo: 'column-chart-container'
-        },
-        title: {
-            text: null
-        },
-        xAxis: {
-            categories: [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            crosshair: true
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [49.9]
-
-        }, {
-            name: 'New York',
-            data: [83.6]
-
-        }, {
-            name: 'London',
-            data: [48.9]
-
-        }, {
-            name: 'Berlin',
-            data: [42.4]
-
-        }]
-    });
-
-    var solidgauge_chart_container = Highcharts.chart('solidgauge-chart-container', {
-
-        chart: {
-            type: 'solidgauge',
-            renderTo: 'solidgauge-chart-container',
-            height: '110%',
-            events: {
-                render: renderIcons
-            }
-        },
-
-        title: {
-            text: null,
-            style: {
-                fontSize: '24px'
-            }
-        },
-
-        tooltip: {
-            borderWidth: 0,
-            backgroundColor: 'none',
-            shadow: false,
-            style: {
-                fontSize: '16px'
-            },
-            valueSuffix: '%',
-            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}</span>',
-            positioner: function (labelWidth) {
-                return {
-                    x: (this.chart.chartWidth - labelWidth) / 2,
-                    y: (this.chart.plotHeight / 2) + 15
-                };
-            }
-        },
-
-        pane: {
-            startAngle: 0,
-            endAngle: 360,
-            background: [{ // Track for Move
-                outerRadius: '112%',
-                innerRadius: '88%',
-                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
-                    .setOpacity(0.3)
-                    .get(),
-                borderWidth: 0
-            }, { // Track for Exercise
-                outerRadius: '87%',
-                innerRadius: '63%',
-                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[1])
-                    .setOpacity(0.3)
-                    .get(),
-                borderWidth: 0
-            }, { // Track for Stand
-                outerRadius: '62%',
-                innerRadius: '38%',
-                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[2])
-                    .setOpacity(0.3)
-                    .get(),
-                borderWidth: 0
-            }]
-        },
-
-        yAxis: {
-            min: 0,
-            max: 100,
-            lineWidth: 0,
-            tickPositions: []
-        },
-
-        plotOptions: {
-            solidgauge: {
-                dataLabels: {
-                    enabled: false
-                },
-                linecap: 'round',
-                stickyTracking: false,
-                rounded: true
-            }
-        },
-
-        series: [{
-            name: 'Move',
-            data: [{
-                color: Highcharts.getOptions().colors[0],
-                radius: '112%',
-                innerRadius: '88%',
-                y: 80
-            }]
-        }, {
-            name: 'Exercise',
-            data: [{
-                color: Highcharts.getOptions().colors[1],
-                radius: '87%',
-                innerRadius: '63%',
-                y: 65
-            }]
-        }, {
-            name: 'Stand',
-            data: [{
-                color: Highcharts.getOptions().colors[2],
-                radius: '62%',
-                innerRadius: '38%',
-                y: 50
-            }]
-        }]
-    });
-
-    var pie_chart_container = Highcharts.chart('pie-chart-container', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie',
-            renderTo: 'pie-chart-container'
-        },
-        title: {
-            text: null
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        accessibility: {
-            point: {
-                valueSuffix: '%'
-            }
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                }
-            }
-        },
-        series: [{
-            name: 'Brands',
-            colorByPoint: true,
-            data: [{
-                name: 'Chrome',
-                y: 61.41,
-                sliced: true,
-                selected: true
-            }, {
-                name: 'Internet Explorer',
-                y: 11.84
-            }, {
-                name: 'Firefox',
-                y: 10.85
-            }, {
-                name: 'Edge',
-                y: 4.67
-            }, {
-                name: 'Safari',
-                y: 4.18
-            }, {
-                name: 'Sogou Explorer',
-                y: 1.64
-            }, {
-                name: 'Opera',
-                y: 1.6
-            }, {
-                name: 'QQ',
-                y: 1.2
-            }, {
-                name: 'Other',
-                y: 2.61
-            }]
-        }]
-    });
-
-});
-
 function renderIcons() {
     // Move icon
     if (!this.series[0].icon) {
@@ -290,5 +57,226 @@ function renderIcons() {
         (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
     );
 }
+
+$(function () {
+    $('#column-chart-container').highcharts({
+        chart: {
+            type: 'column',
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            categories: [
+                'Time',
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            title: null
+        },
+        credits: false,
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: '1',
+            data: [49.9]
+
+        }, {
+            name: '2',
+            data: [83.6]
+
+        }, {
+            name: '3',
+            data: [48.9]
+
+        }, {
+            name: '4',
+            data: [42.4]
+
+        }]
+    });
+    $('#solidgauge-chart-container').highcharts({
+        chart: {
+            type: 'solidgauge',
+            events: {
+                render: renderIcons
+            }
+        },
+
+        title: {
+            text: null,
+            style: {
+                fontSize: '24px'
+            }
+        },
+
+        credits: false,
+        tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'none',
+            shadow: false,
+            style: {
+                fontSize: '16px'
+            },
+            valueSuffix: '%',
+            pointFormat: '{series.name}<br><span style="font-size:1em; color: black; font-weight: bold">{point.y}</span>',
+            positioner: function (labelWidth) {
+                return {
+                    x: (this.chart.chartWidth - labelWidth) / 2,
+                    y: (this.chart.plotHeight / 2) + 15
+                };
+            }
+        },
+
+        pane: {
+            startAngle: 0,
+            endAngle: 360,
+            background: [{ // Track for Move
+                outerRadius: '112%',
+                innerRadius: '88%',
+                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
+                    .setOpacity(0.3)
+                    .get(),
+                borderWidth: 0
+            }, { // Track for Exercise
+                outerRadius: '87%',
+                innerRadius: '63%',
+                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[1])
+                    .setOpacity(0.3)
+                    .get(),
+                borderWidth: 0
+            }, { // Track for Stand
+                outerRadius: '62%',
+                innerRadius: '38%',
+                backgroundColor: Highcharts.color(Highcharts.getOptions().colors[2])
+                    .setOpacity(0.3)
+                    .get(),
+                borderWidth: 0
+            }]
+        },
+
+        yAxis: {
+            min: 0,
+            max: 100,
+            lineWidth: 0,
+            tickPositions: []
+        },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    enabled: false
+                },
+                linecap: 'round',
+                stickyTracking: false,
+                rounded: true
+            }
+        },
+
+        series: [{
+            name: '1',
+            data: [{
+                color: Highcharts.getOptions().colors[0],
+                radius: '112%',
+                innerRadius: '88%',
+                y: 80
+            }]
+        }, {
+            name: '2',
+            data: [{
+                color: Highcharts.getOptions().colors[1],
+                radius: '87%',
+                innerRadius: '63%',
+                y: 65
+            }]
+        }, {
+            name: '3',
+            data: [{
+                color: Highcharts.getOptions().colors[2],
+                radius: '62%',
+                innerRadius: '38%',
+                y: 50
+            }]
+        }]
+    });
+    $('#pie-chart-container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            renderTo: 'pie-chart-container'
+        },
+        title: {
+            text: null
+        },
+        credits: false,
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: '1',
+                y: 61.41,
+                sliced: true,
+                selected: true
+            }, {
+                name: '2',
+                y: 11.84
+            }, {
+                name: '3',
+                y: 10.85
+            }, {
+                name: '4',
+                y: 4.67
+            }, {
+                name: '5',
+                y: 4.18
+            }, {
+                name: '6',
+                y: 1.64
+            }, {
+                name: '7',
+                y: 1.6
+            }, {
+                name: '8',
+                y: 1.2
+            }, {
+                name: '9',
+                y: 2.61
+            }]
+        }]
+    });
+});
 
 
